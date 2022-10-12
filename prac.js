31fa090d19d018cf1e6e50b6ae98de04314beab1
Sowmya-Krish <113003820+Sowmya-Krish@users.noreply.github.com>
2022-10-12 12:13:04 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,9 @@
+function foo(){
+var res=document.getElementById("firstname").value;
+var res1=document.getElementById("Middlename").value;
+var res2=document.getElementById("lastname").value
+var res3=document.getElementById("email").value;
+var res4=document.getElementById("pass").value;
+console.log(res,res1,res2,res3,res4);
+
+}
